856151135019ea673c78e8c3693563d8dab064b6
AlexisPincowscy <alexispincowscy@hotmail.com>
2024-12-28 20:03:30 -0300

    versão final com if/else

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("Olá")
+let nivelHeroi = 9500;
+let nomeHeroi = "Garen";
+if (nivelHeroi <= 1000) {
+    nivelHeroi = "Ferro";   
+    console.log("O Herói " + nomeHeroi + " está no nível " + nivelHeroi);
+} else if (nivelHeroi >= 1001 && nivelHeroi < 2000) {
+    nivelHeroi = "Bronze";  
+    console.log("O Herói " + nomeHeroi + " está no nível " + nivelHeroi);
+} else if (nivelHeroi >= 2001 && nivelHeroi < 5000) {
+    nivelHeroi = "Prata";
+    console.log("O Herói " + nomeHeroi + " está no nível " + nivelHeroi);
+} else if (nivelHeroi >= 5001 && nivelHeroi < 7000) {
+    nivelHeroi = "Ouro";
+    console.log("O Herói " + nomeHeroi + " está no nível " + nivelHeroi);
+} else if (nivelHeroi >= 7001 && nivelHeroi < 8000) {
+    nivelHeroi = "Platina";
+    console.log("O Herói " + nomeHeroi + " está no nível " + nivelHeroi);
+} else if (nivelHeroi >= 8001 && nivelHeroi < 9000) {
+    nivelHeroi = "Ascendente";
+    console.log("O Herói " + nomeHeroi + " está no nível " + nivelHeroi);
+} else if (nivelHeroi >= 9001 && nivelHeroi < 10000) {
+    nivelHeroi = "Imortal";
+    console.log("O Herói " + nomeHeroi + " está no nível " + nivelHeroi);
+} else if (nivelHeroi >= 10001) {
+    nivelHeroi = "Radiante";
+    console.log("O Herói " + nomeHeroi + " está no nível " + nivelHeroi);
+} else {
+        console.log("Nível não encontrado");
+}
+
